4c9785c5a0bb9c530c40b7880cd4def59a374482
Almar Heidarsson <almarheidars@gmail.com>
2016-06-21 18:27:28 -0400

    nodemon, node-debugger

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var express = require('express'),
-    posts = require('./mock/posts.json');
+var posts = require('./mock/posts.json');
 
 var app = express();
 
+debugger;
+
 app.get('/', function(req, res){
-    response.send("<h1>I Love Treehouse!</h1>");
+    res.send("<h1>I Love Treehouse!</h1>");
 });
 
 app.get('/blog', function (req, res){
